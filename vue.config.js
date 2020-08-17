3db846a65053cda64ff76526392abef1eda9b73a
Maarten Vliegenthart <maarten@maximum.nl>
2020-08-17 21:51:57 +0200

    npm update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'development' ? '' : 'fleurslijstje',
+    publicPath: process.env.NODE_ENV === 'development' ? '' : '/fleurslijstje',
     productionSourceMap: false
 };
